abb9a93c725342a43fa1d2d4f6400f95ddd66f4a
zxenia <ksenia1zaytseva@gmail.com>
2019-05-15 15:09:07 +0200

    loader shown on file upload; Thanks to Barbara and Peter

--- vocabs/forms.py
+++ vocabs/forms.py
@@ -36,7 +36,7 @@ class UploadFileForm(forms.Form):
         self.helper.form_class = 'form-horizontal'
         self.helper.label_class = 'col-md-3'
         self.helper.field_class = 'col-md-9'
-        self.helper.add_input(Submit('submit', 'upload'),)
+        self.helper.add_input(Submit('submit', 'upload', css_id='uploadFile'),)
 
 
 def custom_name_errors(field_name):
