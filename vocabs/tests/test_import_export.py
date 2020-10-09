2e7ad23e65a57943921b63052d0d4a0fb084f787
zxenia <ksenia1zaytseva@gmail.com>
2020-10-08 23:45:07 -0400

    add tests for export add test vocab

--- vocabs/tests/test_import_export.py
+++ vocabs/tests/test_import_export.py
@@ -2,13 +2,16 @@ import os
 
 from django.test import TestCase
 from django.contrib.auth.models import User
+from rdflib import Graph
 
 from .constants import USER
 from ..models import SkosConceptScheme, SkosCollection, SkosConcept
 from ..skos_import import SkosImporter
+from ..rdf_utils import graph_construct_qs
 
 
-EXAMPLE_SKOS = os.path.join(os.path.dirname(__file__), "example_skos.rdf")
+EXAMPLE_SKOS_IMPORT = os.path.join(os.path.dirname(__file__), "example_skos_import.rdf")
+EXAMPLE_SKOS_EXPORT = os.path.join(os.path.dirname(__file__), "example_skos_export.rdf")
 
 
 class TestSkosImport(TestCase):
@@ -16,7 +19,7 @@ class TestSkosImport(TestCase):
 
     def setUp(self) -> None:
         self.user = User.objects.create_user(**USER)
-        skos_vocab = SkosImporter(file=EXAMPLE_SKOS, file_format="xml", language="en")
+        skos_vocab = SkosImporter(file=EXAMPLE_SKOS_IMPORT, file_format="xml", language="en")
         self.concept_scheme = skos_vocab.parse_triples()
         skos_vocab.upload_data(self.user)
 
@@ -31,3 +34,17 @@ class TestSkosImport(TestCase):
         self.assertEqual(len(SkosConceptScheme.objects.all()), 1)
         self.assertEqual(len(SkosCollection.objects.all()), 6)
         self.assertEqual(len(SkosConcept.objects.all()), 114)
+
+
+class TestSkosExport(TestCase):
+    """ Test module for SKOS export functionality. """
+
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(**USER)
+        skos_vocab = SkosImporter(file=EXAMPLE_SKOS_EXPORT, file_format="xml", language="en")
+        self.concept_scheme = skos_vocab.parse_triples()
+        skos_vocab.upload_data(self.user)
+
+    def test_skos_export(self):
+        g = graph_construct_qs(SkosConcept.objects.all())
+        self.assertEqual(Graph, type(g))
