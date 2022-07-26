25e6edce28fcca91021b0f0d120e16e9c391a72a
csae8092 <peter.andorfer@oeaw.ac.at>
2022-07-26 12:36:29 +0200

    dump into acdh-oeaw/vocabseditor

--- vocabseditor/settings.py
+++ vocabseditor/settings.py
@@ -185,7 +185,7 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_RESULT_EXTENDED = True
 
 GHPAT = os.environ.get('GHPAT')
-GHREPO = os.environ.get('GHREPO', 'csae8092/whatever')
+GHREPO = os.environ.get('GHREPO', 'acdh-oeaw/vocabseditor')
 
 
 # Django guardian settings
